6ad8b091ec24940350e079742955eb410b9a73d6
Mehedi Hasan Siddique <hasanmehidi2016@gmail.com>
2025-12-17 14:15:25 +0600

    Create 6 Simple Method (Function).java

---
+++ 6 Simple Method (Function).java
@@ -0,0 +1,12 @@
+class MethodExample {
+
+    // Method to add two numbers
+    static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        int result = add(5, 3);
+        System.out.println("Result = " + result);
+    }
+}
