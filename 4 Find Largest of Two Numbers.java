01abd85af2b6ea206aaa1d8441f3c1f49ffe24e3
Mehedi Hasan Siddique <hasanmehidi2016@gmail.com>
2025-12-17 14:14:05 +0600

    Create 4 Find Largest of Two Numbers.java

---
+++ 4 Find Largest of Two Numbers.java
@@ -0,0 +1,12 @@
+class Largest {
+    public static void main(String[] args) {
+        int x = 15;
+        int y = 25;
+
+        if (x > y) {
+            System.out.println("x is larger");
+        } else {
+            System.out.println("y is larger");
+        }
+    }
+}
