7de55a8b7e927ea02f811664d1ae1dd984a5880b
Mehedi Hasan Siddique <hasanmehidi2016@gmail.com>
2025-12-17 14:13:36 +0600

    Create 3 Check Even or Odd.java

---
+++ 3 Check Even or Odd.java
@@ -0,0 +1,12 @@
+class EvenOdd {
+    public static void main(String[] args) {
+        int number = 7;
+
+        // Check if number is even
+        if (number % 2 == 0) {
+            System.out.println("Even Number");
+        } else {
+            System.out.println("Odd Number");
+        }
+    }
+}
